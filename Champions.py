4fafce7e3bb9d14046710fb3ecea745f8dbf4fcf
stylz911218 <emskpt.9192@gmail.com>
2024-09-07 23:25:00 +0800

    3rd commit: finish jungle

--- Champions.py
+++ Champions.py
@@ -24,7 +24,7 @@ top_Aurora = champion("Aurora", ["FF", "Elec"], "top", ["flash", "tp", "ignite"]
 top_Volibear = champion("Volibear", ["GotU", "PTA"], "top", ["flash", "tp", "ignite"])
 top_Tryndamere = champion("Tryndamere", ["GotU", "FF"], "top", ["flash", "ignite", "ghost"])
 top_Mordekaiser = champion("Mordekaiser", ["Con"], "top", ["flash", "tp", "ghost", "ignite"])
-top_Kled = champion("Kled", ["Con"], "top", ["flash", "tp", "ignite"]) # here
+top_Kled = champion("Kled", ["Con"], "top", ["flash", "tp", "ignite"])
 top_Jax = champion("Jax", ["Con", "GotU"], "top", ["flash", "tp", "ignite"])
 top_Yorick = champion("Yorick", ["Con", "AC"], "top", ["flash", "tp", "ghost", "ignite"])
 top_Zac = champion("Zac", ["Con", "GotU", "AS"], "top", ["flash", "tp", "ignite"])
@@ -72,7 +72,62 @@ top_champions = [top_Aatrox, top_Nasus, top_Darius, top_Poppy, top_Camille, top_
                  top_Kyle, top_Chogath
                  ]
 # 打野角色
-jungle_champions = []
+jungle_Leesin = champion("Leesin", ["Con", "Elec", "DH"], "jungle", ["flash", "smite"])
+jungle_Lillia = champion("Lillia", ["Con"], "jungle", ["flash", "smite"])
+jungle_Reksai = champion("Reksai", ["Con"], "jungle", ["flash", "smite"])
+jungle_Noc = champion("Noc", ["Con", "PTA"], "jungle", ["flash", "smite"])
+jungle_Warwick = champion("Warwick", ["PTA"], "jungle", ["flash", "smite", "ghost"])
+jungle_Shaco = champion("Shaco", ["DH", "HoB"], "jungle", ["flash", "smite", "ignite"])
+jungle_Khazix = champion("Khazix", ["FS", "DH", "Elec"], "jungle", ["flash", "smite", "ghost"])
+jungle_Belveth = champion("Belveth", ["Con", "PTA"], "jungle", ["flash", "smite", "ghost"])
+jungle_Xinzhao = champion("Xinzhao", ["Con", "HoB"], "jungle", ["flash", "smite"])
+jungle_Amumu = champion("Amumu", ["Con", "AS"], "jungle", ["flash", "smite"])
+jungle_Elise = champion("Elise", ["DH", "PTA"], "jungle", ["flash", "smite", "ignite"])
+jungle_Yi = champion("YI", ["PTA", "HoB"], "jungle", ["flash", "smite", "ghost"])
+jungle_Skarner = champion("Skarner", ["GotU", "AS"], "jungle", ["flash", "smite", "ghost"])
+jungle_Ekko = champion("Ekko", ["DH"], "jungle", ["flash", "smite", "ignite"])
+jungle_Karthus = champion("Karthus", ["DH"], "jungle", ["flash", "smite", "exhaust"])
+jungle_Taliyah = champion("Taliyah", ["DH"], "jungle", ["flash", "smite", "ghost"])
+jungle_Viego = champion("Viego", ["Con"], "jungle", ["flash", "smite"])
+jungle_Evelyn = champion("Evelyn", ["Elec"], "jungle", ["flash", "smite", "ghost"])
+jungle_Volibear = champion("Volibear", ["PTA"], "jungle", ["flash", "smite", "ghost"])
+jungle_Nidalee = champion("Nidalee", ["Con", "DH"], "jungle", ["flash", "smite"])
+jungle_Gwen = champion("Gwen", ["Con"], "jungle", ["flash", "smite", "ghost"])
+jungle_Jarvan = champion("Gwen", ["Con"], "jungle", ["flash", "smite", "ghost"])
+jungle_Talon = champion("Talon", ["FS", "Elec"], "jungle", ["flash", "smite", "ignite"])
+jungle_Kayn = champion("Kayn", ["Con", "DH"], "jungle", ["flash", "smite", "smite", "exhaust"])
+jungle_Briar = champion("Briar", ["PTA", "HoB"], "jungle", ["flash", "smite", "ignite"])
+jungle_Vi = champion("Vi", ["Con"], "jungle", ["flash", "smite", "ghost"])
+jungle_Graves = champion("Graves", ["FF"], "jungle", ["flash", "smite", "ghost"])
+jungle_Ivern = champion("Ivern", ["SA"], "jungle", ["flash", "smite", "ghost"])
+jungle_Zac = champion("Zac", ["Con", "AS"], "jungle", ["flash", "smite", "ignite"])
+jungle_Fiddlestick = champion("Fiddlestick", ["FS", "Elec", "DH"], "jungle", ["flash", "smite", "ghost"])
+jungle_Nunu = champion("Nunu", ["PR"], "jungle", ["flash", "smite", "ghost"])
+jungle_Udyr = champion("Udyr", ["Con", "PTA"], "jungle", ["flash", "smite", "ghost"])
+jungle_Hekarim = champion("Hekarim", ["Con", "PR"], "jungle", ["flash", "smite", "ghost"])
+jungle_Wukong = champion("Wukong", ["Con"], "jungle", ["flash", "smite", "ghost"])
+jungle_Poppy = champion("Poppy", ["DH", "PR", "AS"], "jungle", ["flash", "smite", "ghost"])
+jungle_Kindred = champion("Kindred", ["Con", "PTA"], "jungle", ["flash", "smite", "ghost"])
+jungle_Zyra = champion("Zyra", ["AC", "Elec"], "jungle", ["flash", "smite", "ghost"])
+jungle_Rammus = champion("Rammus", ["AS"], "jungle", ["flash", "smite", "ghost"])
+jungle_Shyvana = champion("Graves", ["FF", "DH"], "jungle", ["flash", "smite", "ghost"])
+jungle_Diana = champion("Diana", ["Con"], "jungle", ["flash", "smite", "ghost"])
+jungle_Sjuanni = champion("Sjuanni", ["AS"], "jungle", ["flash", "smite", "ghost"])
+jungle_Gragas = champion("Gragas", ["DH"], "jungle", ["flash", "smite"])
+jungle_Brand = champion("Brand", ["DH", "Elec"], "jungle", ["flash", "smite", "ghost"])
+jungle_Zed = champion("Zed", ["FS", "DH", "Elec"], "jungle", ["flash", "smite", "ghost"])
+jungle_Sylas = champion("Sylas", ["DH", "Elec"], "jungle", ["flash", "smite", "ignite"])
+jungle_Rengar = champion("Rengar", ["Con", "Elec", "DH"], "jungle", ["flash", "smite", "ghost"])
+jungle_Maokai = champion("Maokai", ["PR", "AS"], "jungle", ["flash", "smite", "ghost"])
+jungle_Qiyanna = champion("Qiyanna", ["FS", "Elec"], "jungle", ["flash", "smite", "ignite"])
+
+
+jungle_champions = [jungle_Leesin, jungle_Lillia, jungle_Reksai, jungle_Noc, top_Warwick, jungle_Shaco, jungle_Khazix, jungle_Belveth,
+                    jungle_Xinzhao, jungle_Elise, jungle_Amumu, jungle_Yi, jungle_Skarner, jungle_Ekko, jungle_Karthus, jungle_Taliyah,
+                    jungle_Viego, jungle_Evelyn, jungle_Volibear, jungle_Nidalee, jungle_Gwen, jungle_Jarvan, jungle_Talon, jungle_Kayn,
+                    jungle_Briar, jungle_Vi, jungle_Graves, jungle_Ivern, jungle_Zac, jungle_Fiddlestick, jungle_Nunu, jungle_Udyr, top_Wukong,
+                    jungle_Hekarim, jungle_Poppy, jungle_Kindred, jungle_Zyra, jungle_Rammus, jungle_Shyvana, jungle_Diana, jungle_Sjuanni, top_Gragas,
+                    jungle_Brand, jungle_Zed, jungle_Sylas, jungle_Rengar, jungle_Maokai, jungle_Qiyanna]
 
 # 中路角色
 mid_champions = []
